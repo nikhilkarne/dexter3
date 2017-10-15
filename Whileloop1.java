3b3438725256d3e74d68fababba35e87ffead7fd
nikhil karne <nikhil270593@gmail.com>
2017-10-15 13:46:27 -0400

    first file1 to be commited

---
+++ Whileloop1.java
@@ -0,0 +1,28 @@
+package me.training.Nikij2se.basics;
+
+public class Whileloop1 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		i++;
+		while (i < 10) {
+			if(i==5){
+				continue;
+			}
+			
+			//int j = 0;
+			//while (j < 5) {
+			//	System.out.print(i+j+"\t");
+				//j++;
+			System.out.println(i);
+			
+			
+			}
+			
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+
